2549a9c60c4eab46706612ce08ccb1cb8783ec82
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-30 16:16:44 +0600

    syntax of python

---
+++ syntax.py
@@ -0,0 +1,27 @@
+#define main function to print out something 
+#whitespace and identation 
+print("Whitespace and Identation")
+def main():
+    i=1
+    max=10
+    while(i<=max):
+        print(i)
+        i=i+1
+
+#call function main 
+main()
+import keyword
+print(keyword.kwlist)
+print("String literals are single quotes(') double quotes,and triple single quotes")
+s='This is a string'
+print(s)
+s="Another string using double quotes"
+print(s)
+s='''string can span
+     multiple line'''
+print(s)     
+#tThis is a single Line comment in Python
+# print("Continuation of Statements")
+# if(a == True)and (b == False) and \
+#     (c == True):
+#     print("Continutation of Statements")

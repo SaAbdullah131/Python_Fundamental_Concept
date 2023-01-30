2a86984159c14e2efd80c5e77a0c18c2aef2d21b
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-30 17:19:25 +0600

    String and it's concatenation

---
+++ concatenatin_string.py
@@ -0,0 +1,15 @@
+#concatenating string in python
+
+greeting = 'Good'
+time = 'Afternoon'
+
+greeting = greeting + time + '!'
+print(greeting)
+
+#Accesing String Elements 
+str = "Python String"
+print(str[0]) #p
+print(str[1]) #y
+print(str[-1])#s
+
+
---
+++ string.py
@@ -0,0 +1,25 @@
+#Python String
+message = 'This is a string in Python'
+message = "This is also a string"
+message = "It\'s a String"
+print(message)
+message = r'C:\python\bin'
+print(message)
+
+#Creating Multiline strings
+help_message= '''
+usage:mysql command
+    -h hostname
+    -d database name
+    -u username
+    -p password
+'''
+print(help_message)
+name = "John"
+mess = "Hi"
+mess = f'Hi {name}'
+print(mess)
+
+greeting = 'Good' 'Morning!'
+print(greeting)
+

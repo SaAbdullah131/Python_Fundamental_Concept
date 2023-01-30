3323be2b0e7bd112adc3c7e3e6b0606361985114
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-30 16:42:55 +0600

    Variables in python

---
+++ variables.py
@@ -0,0 +1,15 @@
+#variable in python
+#Here message is a variable .It holds string "Hello World"
+
+message = "Hello,World"
+print(message)
+
+message='Good Bye'
+print(message)
+
+print("Average of Mark")
+English=45;
+Bangla=71;
+Math=60;
+Average = (English + Bangla + Math)/3;
+print("%.2f" %Average) 

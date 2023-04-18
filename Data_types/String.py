cc2193f081dc8e4153555c4b18c6034d9408d032
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-04-18 21:55:59 +0600

    String and some of it's operation

---
+++ Data_types/String.py
@@ -0,0 +1,39 @@
+# string in python
+
+
+# double and single quote are work same here
+print("Hello");
+print('Hello');
+
+# Assign string to a variable 
+a = "Hello";
+print(a);
+
+# Multiline String
+h ='''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua. '''
+print(h);
+
+# String Are Array
+vari = "Hello ,World !";
+print(vari[1]);
+
+# Looping through a String
+for r in 'banana':
+    print(r)
+
+# String Length
+print('length is : ',len(vari));
+
+# Check String 
+txt = " The best things in life are free!";
+print('free' in txt); # it return boolean value that means True or False;
+if 'free' in txt:
+    print ("Yes,'free' is present");
+
+print("expensive" not in txt); # expensive not in txt this is true cause expensive does not exist in txt;
+
+if 'expensive' not in txt: 
+    print("No, 'expensive' is Not Present");
---
+++ Data_types/type_casting.py
@@ -0,0 +1,18 @@
+# type casting means covert one type to another types in python 
+# convert float and string to a integer number
+x = int(1); # x will be 1 
+y = int(2.8) ; # y will be 2;
+z = int('3') ; # z will be 3
+
+# converting in float  
+a  = float(1) ; # a will be 1.0
+b = float (2.8) # b will be 2.8
+c = float('3'); # c will be 3.0 
+d = float ('4.3'); # d will be 4.3
+
+# converting in string 
+
+e = str('st1'); # e will be 'st1'
+f = str(2) ; # f will be '2'
+g = str('3.0') ; # g will be '3.0'
+

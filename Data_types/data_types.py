645d7f5b7b6da105e2413ff7e338320d23dc843e
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-04-18 21:31:32 +0600

    data types in python

---
+++ Data_types/data_types.py
@@ -0,0 +1,32 @@
+# data types in python 
+x = 5;
+print(type(x)); # x is a integer types data
+
+y = "Hello World";
+print(type(y)); # string types data . this should be in single or double quotation
+
+z= 20.5;
+print(type(z)); # float types data
+
+a = 1j;
+print(type(a)); # complex types data
+
+fruits = ['apple','banana','cherry'];
+print(type(fruits)); # list types data
+
+fruitsAll = ('apple','banana','cherry');
+print(type(fruitsAll)); # tuple types data using () parenthesis
+
+person  = {'name':'SA Abdullah','Age':24};
+print(type(person)); # dictionary types data where key values exist
+
+vegetable = {'cucumber','pumpkins','photato'};
+print(type(vegetable)); # this is set types data 
+
+froz = frozenset({'apple','banana','cherry'});
+print(type(froz)); # frozenset types data
+
+hello = True; # or False
+print(type(hello)); # boole types data either true or false
+
+

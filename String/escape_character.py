5f41bca419fa5b492b8f25619c89d0bc7b2002a9
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-04-24 23:54:07 +0600

    boolean in py

---
+++ String/escape_character.py
@@ -0,0 +1,7 @@
+# Escape Characters
+
+txt = " We are the so-called \"Vikings\" from the north";
+print(txt);
+
+hello = 'Hello from where \n I am from\t Bangladesh';
+print(hello);
---
+++ boolean.py
@@ -0,0 +1,6 @@
+# boolean python 
+
+print( 10 > 9);
+# this will be return true;
+
+print( 100 ==9) # check Whether equal or not..

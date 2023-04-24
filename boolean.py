0fa60e1870bd2bc1fdbe910645cded8d66f21159
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-04-25 00:00:24 +0600

    boolean type variable in py

--- boolean.py
+++ boolean.py
@@ -4,3 +4,14 @@ print( 10 > 9);
 # this will be return true;
 
 print( 100 ==9) # check Whether equal or not..
+
+print(10 < 9) # false;
+
+a = 200;
+b = 33;
+if b>a :
+    print('b is greater than a');
+else:
+    print('b is not greater than a')
+print(bool('hello')); # any string true 
+print(bool(15));

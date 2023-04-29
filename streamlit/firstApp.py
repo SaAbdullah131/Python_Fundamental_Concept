bfe129a8b366b679887deb6cdfec7a15d8fcf206
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-04-30 04:15:34 +0600

    streamlit run first app in py

---
+++ streamlit/firstApp.py
@@ -0,0 +1,16 @@
+# streamlit first app.
+import streamlit as st;
+#Title
+st.title('My First Streamlit app');
+
+# Header
+st.header('Second Line');
+
+#Markdown
+st.markdown('Hi ! There');
+
+# success text --green
+st.success("YaY ! This wan Success !!!")
+
+# erro text -- red
+st.error('OOPs!! That Wat an Error');

11820711d3360d0cdabdbf48bc3a72eddd51738f
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-05-01 04:06:40 +0600

    streamlit write() in py

--- streamlit/firstApp.py
+++ streamlit/firstApp.py
@@ -1,5 +1,6 @@
 # streamlit first app.
 import streamlit as st;
+import pandas as pd;
 #Title
 st.title('My First Streamlit app');
 
@@ -14,3 +15,13 @@ st.success("YaY ! This wan Success !!!")
 
 # erro text -- red
 st.error('OOPs!! That Wat an Error');
+
+# write in streamlit
+st.write('Hello, *SA Abdullah* :happy')
+
+# st.write() also accepts other data formats
+st.write(1234)
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40],
+}))
